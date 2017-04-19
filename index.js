cbaef3715cb00b0d6fbcd1a14e46d24456a1a440
Luka Popijac <luka.popijac@gmail.com>
2017-04-19 01:38:56 -0700

    rewrite pdf

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 
 /**
 important:
-This algorithm doesn't handle possible singularity issues. In case of singularities,
-result will contain bunch of NaN values.
-example: if one of the gaussians is stuck at only one data point, it will shrink
-to it and variance will converge to zero.
+This algorithm doesn't handle possible singularity issues. In case of
+singularities, result will contain bunch of NaN values.
+Example: If one of the gaussians is stuck at only one data point,
+         it will shrink to it and variances will converge to zero.
 */
 
 //module.exports = class {
@@ -51,29 +51,6 @@ class GMM {
 		
 	}
 };
-
-
-var gmm = new GMM({
-	dimensions: 2,
-	bufferSize: 1000,
-	weights: [1/3, 1/3, 1/3],
-	means: [[3,4],[6,3],[4,6]],
-	covariances: [
-		[[3,0],[0,3]],
-		[[3,0],[0,3]],
-		[[3,0],[0,3]]
-	]	
-});
-
-gmm.addPoint([2,1]);
-gmm.addPoint([10,5]);
-gmm.addPoint([3,7]);
-
-gmm.runEM(1);
-
-console.log(gmm.weights);
-console.log(gmm.means);
-console.log(gmm.covariances);
 
 function runExpectation() {
 	this.tmpArr.fill(0, 0, this.dataLength);
@@ -148,30 +125,88 @@ function runMaximization() {
 	}
 }
 
-// probability density function
-function pdf(x, mean, cov) {
-	let dimensions = x.length;
-	
-	if(dimensions==1) {
-		// TODO
-	} else if(dimensions==2) {
-		let a = cov[0][0], b = cov[0][1];
-		let c = cov[1][0], d = cov[1][1];
-		
-		let det = a*d - b*c;
-		let detInv = 1/det;  // TODO: this is not safe!
-		
-		let t0 = x[0]-mean[0];
-		let t1 = x[1]-mean[1];
-		
-		// mahalanobis^2
-		let mah2 = detInv * (d*t0*t0 - (b+c)*t0*t1 + a*t1*t1);
-		
-		return Math.exp(-.5*mah2) * Math.pow(2*Math.PI, -.5*dimensions) * Math.sqrt(detInv);
+
+
+
+
+const ln2pi = Math.log(2*Math.PI);
+
+function pdf(x, mean, cov) {  // probability density function
+	let d = typeof x == 'number' ? 1 : x.length;
+	let detInv = 1/determinant(cov);     // TODO: this is not safe!
+	let mah2 = detInv * xmuAxmu(adjugate(cov), mean, x);   // mahalanobis^2
+	return Math.sqrt(detInv) * Math.exp(-.5*(mah2 + d*ln2pi));
+}
+
+function xmuAxmu(A, mu, x) {  // calculate (x-mu)'*A*(x-mu)
+	if(typeof x == 'number') return A*(x-mu)*(x-mu);
+	else if(x.length==1) return A[0][0]*(x[0]-mu[0])*(x[0]-mu[0]);
+	else if(x.length==2) {
+		let d0 = x[0]-mu[0], d1 = x[1]-mu[1];
+		return A[0][0]*d0*d0 + (A[0][1]+A[1][0])*d0*d1 + A[1][1]*d1*d1;
+	}
+	let s = 0, n = x.length;
+	let i, j;
+	for(i=0; i<n; i++) for(j=0; j<n; j++) {
+		s += A[i][j]*(x[i]-mu[i])*(x[j]-mu[j]);
+	}
+	return s;
+}
+
+function adjugate(X) {
+	if(typeof X == 'number') return 1;
+	else if(X.length==1) return [1];
+	else if(X.length==2) return [
+		[ X[1][1], -X[0][1]],
+		[-X[1][0],  X[0][0]]
+	];
+	else if(X.length==3) {
+		let a = X[0][0], b = X[0][1], c = X[0][2];
+		let d = X[1][0], e = X[1][1], f = X[1][2];
+		let g = X[2][0], h = X[2][1], i = X[2][2];
+		return [
+			[e*i-f*h, c*h-b*i, b*f-c*e],
+			[f*g-d*i, a*i-c*g, c*d-a*f],
+			[d*h-e*g, b*g-a*h, a*e-b*d]
+		];
 	} else {
-		for(let i=0; i<dimensions; i++) {
-			// TODO
-		}
+		// TODO
 	}
 }
 
+function determinant(X) {
+	if(typeof X == 'number') return X;
+	else if(X.length==1) return X[0][0];
+	else if(X.length==2) return X[0][0]*X[1][1]-X[0][1]*X[1][0];
+	else if(X.length==3) return (
+		X[0][0] * (X[1][1]*X[2][2] - X[1][2]*X[2][1]) +
+		X[0][1] * (X[1][2]*X[2][0] - X[1][0]*X[2][2]) +
+		X[0][2] * (X[1][0]*X[2][1] - X[1][1]*X[2][0])
+	);
+	else {
+		// TODO
+	}
+}
+
+
+var gmm = new GMM({
+	dimensions: 2,
+	bufferSize: 1000,
+	weights: [1/3, 1/3, 1/3],
+	means: [[3,4],[6,3],[4,6]],
+	covariances: [
+		[[3,0],[0,3]],
+		[[3,0],[0,3]],
+		[[3,0],[0,3]]
+	]	
+});
+
+gmm.addPoint([2,1]);
+gmm.addPoint([10,5]);
+gmm.addPoint([3,7]);
+
+gmm.runEM(1);
+
+console.log(gmm.weights);
+console.log(gmm.means);
+console.log(gmm.covariances);
