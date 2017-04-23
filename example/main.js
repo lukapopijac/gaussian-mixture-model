6e655e8068335833da65b5e8dda6296573a4aa2b
Luka Popijac <luka.popijac@gmail.com>
2017-04-23 02:38:12 -0700

    css graph and axes

--- example/main.js
+++ example/main.js
@@ -1,3 +1,41 @@
+'use strict';
+
+let canvas = document.querySelector('.graph canvas');
+/*
+
+
+canvas.addEventListener('click', function(e) {
+	console.log(e.offsetX, e.offsetY);
+	console.log(canvas.width, canvas.height);
+	
+	var wpx = canvas.scrollWidth;
+	var hpx = canvas.scrollHeight;
+	
+	console.log(wpx, hpx, wpx/hpx);
+	
+	var xMin = -100;
+	var xMax = 100;
+});
+*/
+
+
+let container = document.querySelector('.container');
+let canvasWrap = document.querySelector('.graph .canvas-wrapper');
+let yAxis = document.querySelector('.graph .y-axis');
+let axisThick = yAxis.getBoundingClientRect().width;
+let graph = document.querySelector('.graph');
+
+window.addEventListener('resize', function() {
+	var w = canvasWrap.getBoundingClientRect().width | 0;
+	canvasWrap.style.height = w + 'px';
+	yAxis.style.height = w + 'px';
+	//container.style.maxHeight = (w + axisThick) + 'px';
+	//graph.style.height = (w + axisThick) + 'px';
+});
+
+
+
+/*
 var gmm = new GMM({
 	dimensions: 2,
 	bufferSize: 1000,
@@ -19,3 +57,4 @@ gmm.runEM(1);
 console.log(gmm.weights);
 console.log(gmm.means);
 console.log(gmm.covariances);
+*/
